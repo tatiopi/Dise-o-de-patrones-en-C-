0dff92098df453695beabda14787591cde90f36f
Aramos <entrellardat.developer@gmail.com>
2020-04-19 20:46:32 +0200

    mi primer commit

---
+++ Factories/Factories/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories
+{
+    
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+
+    public class Point
+    {
+        /// Factory method
+        public static Point NewCartesianPoint(double x , double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho , double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        private double x, y;
+        /// <summary>
+        /// Iniatilez a point from EITHER cartesian or polar
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="system"></param>
+        //public Point(double a , double b , CoordinateSystem system = CoordinateSystem.Cartesian)
+        //{
+        //    this.x = a;
+        //    this.y = b;
+        //    switch(system)
+        //    {
+        //        case CoordinateSystem.Cartesian:
+        //            x = a;
+        //            y = b;
+        //            break;
+        //        case CoordinateSystem.Polar:
+        //            x = a * Math.Cos(a);
+        //            y = b * Math.Sin(b);
+        //            break;
+        //        default:
+        //            throw new ArgumentOutOfRangeException(nameof(system), system, null);
+        //    }
+        //}
+
+        // ahora lo hacemos privado!
+        public Point (double x , double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            Console.WriteLine(point.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Patron Observer/Patron Observer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPatterns
+{
+    public class FallIllEventArgs
+    {
+        public string Address;
+    }
+
+    public class Person
+    {
+        public void CatchAcold()
+        {
+            FallsIll?.Invoke(this, new FallIllEventArgs { Address = "123 Londo Roand" });
+        }
+        public event EventHandler<FallIllEventArgs> FallsIll;
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.FallsIll += CallDoctor;
+            person.CatchAcold();
+            person.FallsIll -= CallDoctor;
+        }
+
+        private static void CallDoctor(object sender, FallIllEventArgs e)
+        {
+            Console.WriteLine($"A doctor has been called to {e.Address}");
+        }
+    }
+}
